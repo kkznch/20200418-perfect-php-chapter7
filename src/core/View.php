8f4b2b1dff045f8ff99223b7c1ff44bd39e8202b
kkznch <kkznch710md@gmail.com>
2020-06-06 15:27:02 +0900

    [update]Viewクラス作ったよ

---
+++ src/core/View.php
@@ -0,0 +1,46 @@
+<?php
+
+class View
+{
+    protected $base_dir;
+    protected $defaults;
+    protected $layout_variables = [];
+
+    public function __construct($base_dir, $defaults = [])
+    {
+        $this->base_dir = $base_dir;
+        $this->defaults = $defaults;
+    }
+
+    public function setLayoutVar($name, $value)
+    {
+        $this->layout_variables[$name] = $value;
+    }
+
+    public function render($_path, $_variables = [], $_layout = false)
+    {
+        $_file = $this->base_dir . '/' . $_path . '.php';
+        extract(array_merge($this->defaults, $_variables));
+
+        ob_start();
+        ob_implicit_flush(0);
+
+        require $_file;
+
+        $content = ob_get_clean();
+
+        if($_layout){
+            $content = $this->render($_layout, array_merge($this->layout_variables, array(
+                'content' => $content,
+            )
+            ));
+        }
+
+        return $content;
+    }
+
+    public function escape($string)
+    {
+        return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
+    }
+}
